28a8ab1c52780f3a20d96bbdbbbbaf90f0d4c229
hx <hxfirefox@sina.com>
2016-04-09 23:00:34 +0800

    complete fizzbuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,21 @@
-public class FizzBuzz implements Rule{
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class FizzBuzz implements Rule {
+    private static List<Rule> rules = Lists.newArrayList();
+
+    static {
+        rules.add(new FizzRule());
+        rules.add(new BuzzRule());
+    }
+
     @Override
     public String transform(int number) {
-        return String.valueOf(number);
+        String result = "";
+        for (Rule rule : rules) {
+            result += rule.transform(number);
+        }
+        return result.length() == 0 ? String.valueOf(number) : result;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -21,4 +21,34 @@ public class FizzBuzzTest {
         // then
         assertThat(result, is("1"));
     }
+
+    @Test
+    public void should_out_fizz_when_input_3() throws Exception {
+        // given
+
+        // when
+        final String result = fizzbuzz.transform(3);
+        // then
+        assertThat(result, is("Fizz"));
+    }
+
+    @Test
+    public void should_out_buzz_when_input_5() throws Exception {
+        // given
+
+        // when
+        final String result = fizzbuzz.transform(5);
+        // then
+        assertThat(result, is("Buzz"));
+    }
+
+    @Test
+    public void should_out_fizzbuzz_when_input_15() throws Exception {
+        // given
+
+        // when
+        final String result = fizzbuzz.transform(15);
+        // then
+        assertThat(result, is("FizzBuzz"));
+    }
 }
